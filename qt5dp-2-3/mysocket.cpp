738c2280ff14e4243c1773e955ae89564aff10d7
Adalbert <a.soborka@eks-gmbh.com>
2023-04-17 10:16:54 +0200

    init commit

---
+++ qt5dp-2-3/mysocket.cpp
@@ -0,0 +1,11 @@
+#include "mysocket.h"
+
+MySocket::MySocket(QObject *parent)
+    : QObject{parent}
+{
+
+}
+
+void MySocket::close() {
+    qInfo
+}
---
+++ qt5dp-2-41/airplane.h
@@ -0,0 +1,30 @@
+
+#ifndef AIRPLANE_H
+#define AIRPLANE_H
+
+
+#include <QObject>
+#include <QDebug>
+#include "interfaces/iFlying.h"
+#include "interfaces/iMachine.h"
+
+class AirPlane : public QObject, public iFlying, public iMachine
+{
+    Q_OBJECT
+public:
+    explicit AirPlane(QObject *parent = nullptr);
+
+signals:
+
+
+    // iMachine interface
+public:
+    void refuel();
+
+    // iFlying interface
+public:
+    void fly();
+    void land();
+};
+
+#endif // AIRPLANE_H
